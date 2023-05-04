f964167ce29fef3062bf76d4bf238c5c85a92921
Oleksandr Barabash <alexxbarik@gmail.com>
2023-05-04 22:52:29 +0300

    додає js

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
 // Тут додаємо імпорт готового модулю за зразком:
 
 // import * as moduleName from './js/module-name';
+import * as oopsnotification from './oopsnotification';
---
+++ src/js/oopsnotification.js
@@ -0,0 +1,12 @@
+
+const notificationBtnClose = document.querySelector(`.notification-btn-close`);
+const notification = document.querySelector(`.notification`);
+notificationBtnClose.addEventListener(`click`, onNotificationBtnClick);
+
+
+function  onNotificationBtnClick (){
+    console.log(5);
+}
+    
+
+
