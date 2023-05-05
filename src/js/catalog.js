1aea80f30eaf7591c4230bbc989ddb664d3eb921
MargoMarm <margogerasimenko@gmail.com>
2023-05-06 01:09:18 +0200

    add raiting stars

--- src/js/catalog.js
+++ src/js/catalog.js
@@ -24,13 +24,10 @@ function makeCard({
   first_air_date,
   vote_average,
 }) {
-  console.log(first_air_date, release_date);
-
   const arrOfGenres = getNameOfGenresById(genre_ids);
   const stringOfGenres = arrOfGenres.slice(0, 2).join(', ');
-  const date = release_date || first_air_date;
-
-  initRatings(vote_average);
+	const date = release_date || first_air_date;
+	
 
   return `<li class="catalog__card" data-id="${id}">
     <div class="catalog__img-wrapper">
@@ -43,42 +40,16 @@ function makeCard({
       <ul class="info__list">
       <li class="info__descr">${stringOfGenres}</li>
       <li class="info__descr">${convertReleaseDate(date)}</li>
-      </ul>
 		<div class="catalog__stars-wrap">
-		<div class="catalog__raitnig-active"></div>
-		<div class="catalog__raitnig-list"></div>
+		<div class="catalog__raitnig-active" width="${vote_average / 2 / 0.05}%"></div>
+		
 		</div>
+      </ul>
+		
     </div>
   </li>`;
 }
 
-
-function initRatings(n) {
-  let raitingActive;
-  const value = n;
- const raitings = document.querySelectorAll('.catalog__stars-wrap');
-  for (let index = 0; index < raitings.length; index += 1) {
-    const raiting = raitings[index];
-    console.log(raiting);
-    initRaiting(raiting);
-  }
-
-  function initRaiting(raiting) {
-    initRaitingVars(raiting);
-
-    setRaitingWidth(value);
-  }
-
-  function initRaitingVars(raiting) {
-    raitingActive = raiting.querySelector('.catalog__raitnig-active');
-    console.log(raitingActive);
-  }
-
-  function setRaitingWidth(value) {
-    const raitngActiveWidth = value / 2 / 0.05;
-    raitingActive.style.width = `${raitngActiveWidth}%`;
-  }
-}
 
 function convertReleaseDate(date) {
   if (date) {
