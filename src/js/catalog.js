a04977c612a48f9032429731a4cc5a5ff8e38eaf
MargoMarm <margogerasimenko@gmail.com>
2023-05-06 00:09:35 +0200

    some updates

--- src/js/catalog.js
+++ src/js/catalog.js
@@ -5,24 +5,34 @@ import { setGenresInStorage, getNameOfGenresById } from './get-genres';
 // import * as moduleName from './js/module-name';
 
 setGenresInStorage();
-console.log(sprite);
-export async function creatMarkupCatalogCard(data) {
-  const list = data
-    .map(
-      ({
-        id,
-        poster_path,
-        title,
-        name,
-        genre_ids,
-        release_date,
-        first_air_date,
-      }) => {
-        const arrOfGenres = getNameOfGenresById(genre_ids);
-        const stringOfGenres = arrOfGenres.slice(0, 2).join(', ');
-        const date = release_date || first_air_date;
 
-        return `<li class="catalog__card" data-id="${id}">
+export async function creatMarkupCatalogCard(data) {
+  console.log(data);
+  const markUp = data
+    .slice(0, 10)
+    .reduce((markup, film) => markup + makeCard(film), '');
+  return markUp;
+}
+
+function makeCard({
+  id,
+  poster_path,
+  title,
+  name,
+  genre_ids,
+  release_date,
+  first_air_date,
+  vote_average,
+}) {
+  console.log(first_air_date, release_date);
+
+  const arrOfGenres = getNameOfGenresById(genre_ids);
+  const stringOfGenres = arrOfGenres.slice(0, 2).join(', ');
+  const date = release_date || first_air_date;
+
+  initRatings(vote_average);
+
+  return `<li class="catalog__card" data-id="${id}">
     <div class="catalog__img-wrapper">
       <img src="https://image.tmdb.org/t/p/w500${
         poster_path || 'Oops. There is no poster to this movie'
@@ -31,29 +41,44 @@ export async function creatMarkupCatalogCard(data) {
     <div class="catalog__info info">
       <p class="info__title">${name || title}</p>
       <ul class="info__list">
-        <li class="info__descr">${stringOfGenres}</li>
-        <li class="info__descr">${convertReleaseDate(date)}</li>
-
-		  
-<svg width="20" height="20">
-              <use href="${sprite}#icon-star-full"></use>
-            </svg>
-</div>
+      <li class="info__descr">${stringOfGenres}</li>
+      <li class="info__descr">${convertReleaseDate(date)}</li>
       </ul>
+		<div class="catalog__stars-wrap">
+		<div class="catalog__raitnig-active"></div>
+		<div class="catalog__raitnig-list"></div>
+		</div>
     </div>
   </li>`;
-      }
-    )
-    .join('');
-  return list;
 }
 
-function makeStars(n) {
-  console.log(n / 2);
-  const numberOfStars = Math.round(n / 2);
-  console.log(numberOfStars);
+
+function initRatings(n) {
+  let raitingActive;
+  const value = n;
+ const raitings = document.querySelectorAll('.catalog__stars-wrap');
+  for (let index = 0; index < raitings.length; index += 1) {
+    const raiting = raitings[index];
+    console.log(raiting);
+    initRaiting(raiting);
+  }
+
+  function initRaiting(raiting) {
+    initRaitingVars(raiting);
+
+    setRaitingWidth(value);
+  }
+
+  function initRaitingVars(raiting) {
+    raitingActive = raiting.querySelector('.catalog__raitnig-active');
+    console.log(raitingActive);
+  }
+
+  function setRaitingWidth(value) {
+    const raitngActiveWidth = value / 2 / 0.05;
+    raitingActive.style.width = `${raitngActiveWidth}%`;
+  }
 }
-makeStars(7.44);
 
 function convertReleaseDate(date) {
   if (date) {
--- src/js/render-catalog.js
+++ src/js/render-catalog.js
@@ -4,30 +4,29 @@ import * as Api from './api';
 
 const catalogRef = document.querySelector('.catalog');
 const input = document.querySelector('.catalog__input');
-console.log(input);
 
 input.addEventListener('input', onInputSearch);
 
 async function onInputSearch(e) {
-	clearMarkup(catalogRef);
-	try {
-		const search = e.currentTarget.value;
-		console.log(search);
-		
-		const searchData = await Api.getBySearch(search, 1);
-		const searchResult = searchData.results;
+  clearMarkup(catalogRef);
+  try {
+    const search = e.currentTarget.value;
+    console.log(search);
 
-		creatMarkupCatalogCard(searchResult).then(
-			data => (catalogRef.innerHTML = data)
-		);
-	} catch (error) {
-		console.log(error);
-	}
+    const searchData = await Api.getBySearch(search, 1);
+    const searchResult = searchData.results;
+
+    creatMarkupCatalogCard(searchResult).then(
+      data => (catalogRef.innerHTML = data)
+    );
+  } catch (error) {
+    console.log(error);
+  }
 }
-  Api.getWeekTrending(1).then(data => {
-    const films = data.results;
+Api.getWeekTrending(1).then(data => {
+  const films = data.results;
 
-    creatMarkupCatalogCard(films)
-      .then(data => (catalogRef.innerHTML = data))
-      .catch(error => console.log(error));
-  });
+  creatMarkupCatalogCard(films)
+    .then(data => (catalogRef.innerHTML = data))
+    .catch(error => console.log(error));
+});
