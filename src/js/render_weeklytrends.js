89fb5810d69c67800e82967c8700c2362b2d1489
TaurusVB <114020229+TaurusVB@users.noreply.github.com>
2023-05-06 17:18:17 +0300

    add styles and functions for rendering markup

--- src/js/render_weeklytrends.js
+++ src/js/render_weeklytrends.js
@@ -1,7 +1,9 @@
+import sprite from '../../src/images/sprite-stars.svg';
+import { setGenresInStorage, getNameOfGenresById } from './get-genres';
 import * as Api from './api';
 // import * as moduleName from './js/module-name';
 
-const filmList = document.querySelector('.weekly-films');
+const filmList = document.querySelector('.catalog');
 
 Api.getWeekTrending(1).then(data => {
   const films = data.results.slice(0, 3);
@@ -10,62 +12,68 @@ Api.getWeekTrending(1).then(data => {
     .catch(error => console.log(error));
 });
 
-function getNameOfGenresById(ids) {
-  const savedGenres = localStorage.getItem('genres');
-  const parsedGenres = JSON.parse(savedGenres);
-
-  const nameOfGenres = ids.map(id => {
-    const filmById = parsedGenres.find(film => {
-      return film.id === id;
-    });
-    if (!filmById) return '';
-    return filmById.name;
-  });
-  return nameOfGenres;
-}
-
-function setGenresInStorage() {
-  Api.getGanres().then(data => {
-    localStorage.setItem('genres', JSON.stringify(data.genres));
-  });
-}
-
 setGenresInStorage();
 
-export async function creatMarkupCatalogCard(data) {
-  const list = data
-    .map(
-      ({
-        backdrop_path,
-        title,
-        name,
-        genre_ids,
-        release_date,
-        first_air_date,
-      }) => {
-        const arrOfGenres = getNameOfGenresById(genre_ids);
-        const stringOfGenres = arrOfGenres.slice(0, 2).join(', ');
+async function creatMarkupCatalogCard(data) {
+  const markUp = data
+    .slice(0, 3)
+    .reduce((markup, film) => markup + makeCard(film), '');
+  return markUp;
+}
 
-        return `<li class="catalog__card">
+function makeCard({
+  id,
+  poster_path,
+  title,
+  name,
+  genre_ids,
+  release_date,
+  first_air_date,
+  vote_average,
+}) {
+  const arrOfGenres = getNameOfGenresById(genre_ids);
+  const stringOfGenres = arrOfGenres.slice(0, 2).join(', ');
+  const date = release_date || first_air_date;
+
+  return `<li class="catalog__card" data-id="${id}">
     <div class="catalog__img-wrapper">
-      <img src="https://image.tmdb.org/t/p/w500${backdrop_path}" alt="${
-          name || title
-        }" width="395" height="574" class="catalog__img" />
+      <img src="https://image.tmdb.org/t/p/w500${
+        poster_path || 'Oops. There is no poster to this movie'
+      }" alt="${name || title}" width="395" height="574" class="catalog__img" />
     </div>
     <div class="catalog__info info">
       <p class="info__title">${name || title}</p>
       <ul class="info__list">
-        <li class="info__descr">${stringOfGenres}</li>
-        <li class="info__descr">${
-          release_date ? release_date.slice(0, 4) : first_air_date.slice(0, 4)
-        }</li>
+      <li class="info__descr">${stringOfGenres}</li>
+      <li class="info__descr">${convertReleaseDate(date)}</li>
+		<div class="catalog__stars-wrap">
+		<div class="catalog__rating-active" style="width:${
+      vote_average / 2 / 0.05
+    }%"></div>
+		</div>
       </ul>
+		
     </div>
   </li>`;
-      }
-    )
-    .join('');
-  return list;
+}
+
+function convertReleaseDate(date) {
+  if (date) {
+    return date.slice(0, 4);
+  } else return '';
+}
+
+function makeStars(n) {
+  console.log(n / 2);
+  const numberOfStars = Math.round(n / 2);
+  return numberOfStars;
+}
+makeStars(7.44);
+
+function convertReleaseDate(date) {
+  if (date) {
+    return date.slice(0, 4);
+  } else return '';
 }
 
 function clearMarkup(element) {
