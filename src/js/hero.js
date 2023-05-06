c6fe0990318a78d1206823b59b586cbfb18927b9
Yuraro <romanykyriy@gmail.com>
2023-05-06 12:15:43 -0700

    update

--- src/js/hero.js
+++ src/js/hero.js
@@ -1,18 +1,13 @@
-import axios from "axios";
-import { API_KEY } from "./config";
+import { getWeekTrending} from './api.js';
 
-const MAIN_URL = 'https://api.themoviedb.org/3';
 
-export async function getTrending(page = 1) {
-    const url = `${MAIN_URL}/trending/all/day/?api_key=${API_KEY}&language=en-US&page=${page}`;
-    try {
-        const response = await axios.get(url);
-        if (response.status !== 200) {
-            throw new Error(response.status);
-        }
-        return response.data;
-    } catch (error) {
-        console.error(error);
-        throw error;
-    }
+async function updateHero() {
+    const data = await getWeekTrending();
+    const trending = data.results[0];
+    if (!trending) return;
+
+    heroTitle.textContent = trending.title;
+    heroDescription.textContent = trending.overview;
 }
+
+
