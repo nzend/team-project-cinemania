a6db7b08228dcca1e2379ee1e65b92aa44e0d098
Illia Komissarov <iliakomissarow99@gmail.com>
2023-05-07 17:07:38 +0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 // Тут додаємо імпорт готового модулю за зразком:
-import * as code from './mobile-menu';
+import * as mobilMenu from './mobile-menu';
 // import * as moduleName from './js/module-name';
 import * as Catalog from './render-catalog'
 
