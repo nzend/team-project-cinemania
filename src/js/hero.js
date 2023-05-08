4518654db8c6a839c792a75b19cd6a21ff51b73b
Yuraro <romanykyriy@gmail.com>
2023-05-08 01:43:45 -0700

    update

--- src/js/hero.js
+++ src/js/hero.js
@@ -46,14 +46,18 @@ async function getRandomPageData() {
 
 function createTrendingMarkup(movieOfDay) {
     const markup = `
-    <div class="container">
+    <div class="hero-wrap">
+        <div class="background-image">
+            <img src="https://image.tmdb.org/t/p/w500${movieOfDay.backdrop_path}" alt="Hero image">
+        </div>
         <h1 class="title">${movieOfDay.title || movieOfDay.name}</h1>
         <div class="catalog__stars-wrap">
-            <div class="catalog__rating-active" style="width:${vote_average / 2 / 0.05}%">
-            </div>
-        </div>
+            <div class="catalog__rating-active" style="width:${movieOfDay.vote_average / 2 / 0.05}%"></div>
+        </div> 
         <p class="description">${movieOfDay.overview}</p>
         <button class="button-mob">Watch trailer</button>
-    </div>`
+    </div>
+    `
+/*     hero.style.backgroundImage = `url('https://image.tmdb.org/t/p/w500${movieOfDay.backdrop_path}')`; */
     hero.innerHTML = markup;
 }
