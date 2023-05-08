c9adeb1888cb370037a5e8cd0ceafd8384ba8329
Illia Komissarov <iliakomissarow99@gmail.com>
2023-05-08 15:59:20 +0300

    links color are now works great!

--- src/js/switcher.js
+++ src/js/switcher.js
@@ -8,3 +8,16 @@ isLight = !isLight;
 document.body.classList.toggle('light', isLight);
 localStorage.setItem('isLight', isLight);
 };
+
+// page link color
+
+const currentUrl = window.location.href;
+
+// Отримати всі елементи посилань у списку
+const navLink = document.querySelectorAll('.mobile-menu__link, .header__nav-link');
+
+navLink.forEach(function(link) {
+  if (link.href === currentUrl) {
+    link.classList.add('current__nav-link');
+  }
+});
