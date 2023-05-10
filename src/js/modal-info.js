dcc30005a0310cd89adb2d9b9e7869ac6e679f73
nzend <115723110+nzend@users.noreply.github.com>
2023-05-10 21:16:15 +0300

    Update modal-info.js

--- src/js/modal-info.js
+++ src/js/modal-info.js
@@ -1,6 +1,7 @@
 import { getInfoMovie } from './api';
 import { getAddedMovies, setAddedMovies } from './local-storage';
 import { makeCard, myLibGallery } from './added-movies-render';
+import sprite from '../images/sprite.svg';
 
 //* INTERACTION WITH CATALOG
 document.querySelector('.catalog').addEventListener('click', onCatalogClick);
