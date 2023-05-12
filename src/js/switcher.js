291d9e4d5987307e3f3a7cbce185653222d7201b
nzend <115723110+nzend@users.noreply.github.com>
2023-05-12 16:35:30 +0300

    Передаю гілку Владу

--- src/js/switcher.js
+++ src/js/switcher.js
@@ -4,20 +4,21 @@ let isLight = localStorage.getItem('isLight') === 'true';
 document.body.classList.toggle('light', isLight);
 
 LightSwitcher.onclick = function () {
-isLight = !isLight;
-document.body.classList.toggle('light', isLight);
-localStorage.setItem('isLight', isLight);
+  isLight = !isLight;
+  document.body.classList.toggle('light', isLight);
+  localStorage.setItem('isLight', isLight);
 };
 
 // page link color
 
 const currentUrl = window.location.href;
 
+const navLink = document.querySelectorAll(
+  '.mobile-menu__link, .header__nav-link'
+);
 
-const navLink = document.querySelectorAll('.mobile-menu__link, .header__nav-link');
-
-navLink.forEach(function(link) {
-  if (link.href === currentUrl) {
-    link.classList.toggle('current__nav-link');
-  }
-});
+// if (currentUrl.includes('index')) {
+//   classList.toggle('current__nav-link');
+// } else if (currentUrl.includes('catalog')) {
+//   link.classList.toggle('current__nav-link');
+// }
